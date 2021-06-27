6f0231d2b30d1c3282dfcee45f9224b250b1bb54
Koshe26 <86576427+Koshe26@users.noreply.github.com>
2021-06-28 00:41:08 +0200

    Add files via upload

---
+++ __resource.lua
@@ -0,0 +1,5 @@
+--hecho por KOSHE
+--Global leaks = https://discord.gg/YKZmmZG6UQ
+
+client_script 'client.lua'
+server_script 'server.lua'
---
+++ server.lua
@@ -0,0 +1,5 @@
+print("")
+print("^Hecho por KOSHE#6969")
+print("^Global Leaks^0")
+print("^6Discord: https://discord.gg/YKZmmZG6UQ")
+print("")
